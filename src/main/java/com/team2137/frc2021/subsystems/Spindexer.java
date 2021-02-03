a6206aab681b4fd848c66ddf561beb8f20ceaa1f
Andrew Haselton <25783@oxfordwildcats.org>
2021-02-03 18:52:06 -0500

    Create spindexer subsystem (no code)

---
+++ src/main/java/com/team2137/frc2021/subsystems/Spindexer.java
@@ -0,0 +1,10 @@
+package com.team2137.frc2021.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Spindexer extends SubsystemBase {
+
+    public Spindexer() {
+        
+    }
+}
