3995ecb9862e358fe17327946ca2e215afb8fb2a
GenericallyNamed <Statefan0826@gmail.com>
2021-02-10 20:23:46 -0500

    Updated the intake code for TalonSRX

--- src/main/java/com/team2137/frc2021/subsystems/Intake.java
+++ src/main/java/com/team2137/frc2021/subsystems/Intake.java
@@ -3,6 +3,9 @@ package com.team2137.frc2021.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.team2137.frc2021.Constants;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,19 +13,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
-    CANSparkMax motor;
+    //CANSparkMax motor;
+    TalonSRX motor;
+    
     DoubleSolenoid cylinder;
 
     private IntakeState state;
 
     public Intake() {
-        this.motor = new CANSparkMax(Constants.Intake.motorID, MotorType.kBrushless);
+        this.motor = new TalonSRX(Constants.Intake.motorID);
+        //CANSparkMax(Constants.Intake.motorID, MotorType.kBrushless);
         this.cylinder = new DoubleSolenoid(Constants.Intake.cylinderForwardID, Constants.Intake.cylinderReverseID);
     }
     
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("IntakeSpeed", motor.get());
+        SmartDashboard.putNumber("IntakeSpeed", motor.getActiveTrajectoryVelocity());
         SmartDashboard.putString("Cylinder State", cylinder.get().toString());
     }
 
@@ -31,7 +37,7 @@ public class Intake extends SubsystemBase {
     }
 
     public void setMotorPowerRaw(double power) {
-        motor.set(power);
+        motor.set(ControlMode.PercentOutput, power);;
     }
 
     public void setIntakeState(IntakeState state) {
