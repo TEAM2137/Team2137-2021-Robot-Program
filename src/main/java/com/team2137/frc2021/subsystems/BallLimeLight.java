2fbb5b8d2c585d91b99742e5ff09042231e74572
Wyatt Ashley <26737@oxfordwildcats.org>
2021-04-02 19:31:34 -0400

    Ball LimeLight Logic

--- src/main/java/com/team2137/frc2021/subsystems/BallLimeLight.java
+++ src/main/java/com/team2137/frc2021/subsystems/BallLimeLight.java
@@ -1,6 +1,7 @@
 package com.team2137.frc2021.subsystems;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -10,49 +11,51 @@ import java.util.Map.*;
 public class BallLimeLight extends LimeLight {
 
     private final HashMap<Translation2d, Path> limelightSampleSpace = new HashMap<>();
+//    private final HashMap<Path, Translation2d> limelightSampleSpace = new HashMap<>();
 
     public BallLimeLight() {
-        super(NetworkTableInstance.getDefault().getTable("limelight-2"));
-        limelightSampleSpace.put(new Translation2d(-17.3, -10.7), Path.A_Red);
-        limelightSampleSpace.put(new Translation2d(-26.0, -1.75), Path.A_Blue);
-        limelightSampleSpace.put(new Translation2d(16.3, -9.8), Path.B_Red);
-        limelightSampleSpace.put(new Translation2d(-16.3, -1.4), Path.B_Blue);
+        super(NetworkTableInstance.getDefault().getTable("limelight-two"));
+        limelightSampleSpace.put(new Translation2d(17.3, -10.7), Path.A_Red);
+        limelightSampleSpace.put(new Translation2d(26.0, -1.75), Path.A_Blue);
+        limelightSampleSpace.put(new Translation2d(-16.3, -9.8), Path.B_Red);
+        limelightSampleSpace.put(new Translation2d(16.3, -1.4), Path.B_Blue);
     }
 
     @Override
     public void periodic() {
         SmartDashboard.putString("Current Path", getClosestPath().toString());
+
     }
 
     //Maybe look at this change that allows for more data to be added to the sample space and it more RAM conservative and less complicated
     public Path getClosestPath() {
-        Translation2d value = new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY));
+        Translation2d value = new Translation2d(Math.toDegrees(getLimeLightValue(LimeLightValues.TX)), Math.toDegrees(getLimeLightValue(LimeLightValues.TY)));
         return Collections.min(limelightSampleSpace.entrySet(), (o1, o2) -> {
             return (int) Math.ceil(o1.getKey().getDistance(value) - o2.getKey().getDistance(value));
         }).getValue();
     }
 
-    public Path getCurrentPath() {
-        var map = new HashMap<Path, Double>();
+//    public Path getCurrentPath() {
+//        var map = new HashMap<Path, Double>();
+//
+//        map.put(Path.A_Red, A_Red_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
+//        map.put(Path.A_Blue, A_Blue_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
+//        map.put(Path.B_Red, B_Red_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
+//        map.put(Path.B_Blue, B_Blue_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
+//
+//        Entry<Path, Double> min = Collections.min(map.entrySet(), new Comparator<>() {
+//            public int compare(Entry<Path, Double> entry1, Entry<Path, Double> entry2) {
+//                return entry1.getValue().compareTo(entry2.getValue());
+//            }
+//        });
+//
+//        return min.getKey();
+//    }
 
-        map.put(Path.A_Red, A_Red_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
-        map.put(Path.A_Blue, A_Blue_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
-        map.put(Path.B_Red, B_Red_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
-        map.put(Path.B_Blue, B_Blue_Point.getDistance(new Translation2d(getLimeLightValue(LimeLightValues.TX), getLimeLightValue(LimeLightValues.TY))));
-
-        Entry<Path, Double> min = Collections.min(map.entrySet(), new Comparator<>() {
-            public int compare(Entry<Path, Double> entry1, Entry<Path, Double> entry2) {
-                return entry1.getValue().compareTo(entry2.getValue());
-            }
-        });
-
-        return min.getKey();
-    }
-
-    public static final Translation2d A_Red_Point = new Translation2d(-17.3, -10.7);
-    public static final Translation2d A_Blue_Point = new Translation2d(-26, -1.75);
-    public static final Translation2d B_Red_Point = new Translation2d(16.3, -9.8);
-    public static final Translation2d B_Blue_Point = new Translation2d(-16.3, -1.4);
+//    public static final Translation2d A_Red_Point = new Translation2d(-17.3, -10.7);
+//    public static final Translation2d A_Blue_Point = new Translation2d(-26, -1.75);
+//    public static final Translation2d B_Red_Point = new Translation2d(16.3, -9.8);
+//    public static final Translation2d B_Blue_Point = new Translation2d(-16.3, -1.4);
 
     public enum Path {
         A_Red,
